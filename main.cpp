0cdb09a4f5ab4a0b900b6b9d7c1600b392c4c017
muhammad-Umair-80 <muhammadumairkhan417@gmail.com>
2025-05-09 11:07:42 -0700

    create a child from parent class(chatbot)
    create a run function which is called from the main function and perform all the work in it.

--- main.cpp
+++ main.cpp
@@ -18,4 +18,26 @@ class chatbot{
         }
         infile.close();
     }
+};
+class UniversityChatbot : public chatbot{
+    public:
+    void run(){
+        while (true){
+            cout<< "\nWelcome to the University Chatbot! \n";
+            cout<< "1. Department information \n";
+            cout<< "2. Campus facilities \n";
+            cout<< "3. Faculty information \n";
+            cout << "4. Admission Info\n";
+            cout << "5. Class Timings\n";
+            cout << "6. Rules & Regulations\n";
+            cout << "7. Contact & Location\n";
+            cout << "0. Exit\n";
+            cout << "Enter your choice: ";
+            
+            int choice;
+            cin >> choice;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        }
+    }
 }
