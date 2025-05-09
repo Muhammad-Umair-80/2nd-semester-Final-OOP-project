c36efb848660cd3e4bef59662edc2f91f7ec922b
muhammad-Umair-80 <muhammadumairkhan417@gmail.com>
2025-05-09 11:01:53 -0700

    create a class by name chatbot
    create a class through which the code access the file store in the given path.

--- main.cpp
+++ main.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <limits>
 using namespace std;
+class chatbot{
+    protected:
+    void displayFromFile(const string& filename){
+        ifstream file(filename);
+        if (!infile){
+            cerr << "Error opening file: " << filename << endl;
+            return;
+        }
+        string line;
+        while (getline(infile, line)) {
+            cout << line << endl;
+        }
+        infile.close();
+    }
+}
